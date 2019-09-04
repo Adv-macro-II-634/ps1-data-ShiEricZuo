fdb23f97aba20a58f5a8a4c79f0099a5926687d9
Shi Zuo <szuo1@binghamton.edu>
2019-09-04 14:00:20 -0400

    Update analysis.m

--- analysis/code/analysis.m
+++ analysis/code/analysis.m
@@ -1,4 +1,4 @@
 %quantile calculation
-incquan = quantile(income,[0,0.01,0.05,0.1,0.2,0.4,0.6,0.8,0.9,0.95,0.99,1])
-weaquan = quantile(wealth,[0,0.01,0.05,0.1,0.2,0.4,0.6,0.8,0.9,0.95,0.99,1])
-earnquan = quantile(earnings,[0,0.01,0.05,0.1,0.2,0.4,0.6,0.8,0.9,0.95,0.99,1])
+incquan = wprctile(income,[0,1,5,10,20,40,60,80,90,95,99,100],weight)
+weaquan = wprctile(wealth,[0,1,5,10,20,40,60,80,90,95,99,100],weight)
+earnquan = wprctile(earnings,[0,1,5,10,20,40,60,80,90,95,99,100],weight)
