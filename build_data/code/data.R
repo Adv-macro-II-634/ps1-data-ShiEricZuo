028ab844534af6ed33967be64e9126de2f982088
Shi Zuo <szuo1@binghamton.edu>
2019-09-05 01:07:34 -0400

    Update data.R

--- build_data/code/data.R
+++ build_data/code/data.R
@@ -1,3 +1,4 @@
+scf<-data.frame(SCFP2007_2)
 # The summary table of 2007 gives the amount adjusted to 2016 dollar.
 # Using the inflation calculator given by BIS (from July 2016 to July 2007), the inflation factor of 1.16 is applied here to switch the value
 # in summary table back to the 2007 price level.
